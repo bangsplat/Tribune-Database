cd0670e252971fbdb795c7b81e16955445cdf9dd
Bangsplat <theron@dankerton.net>
2013-01-03 15:51:33 -0800

    first commit

---
+++ parseTribuneDatabase.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+
+#
+# parseTribuneDatabase.pl
+#
+# parse Tribune database XML into tab-delimited text file
+#
+# version 0
+# created 2013-01-03
+# modified 2013-01-03
+#
+
+
+
+
+
+
+
+
