89f33c9cafae602abcf4289ea1e80fef13a0108f
Bangsplat <theron@dankerton.net>
2013-01-03 16:13:30 -0800

    Adding options and debug stub

--- parseTribuneDatabase.pl
+++ parseTribuneDatabase.pl
@@ -10,6 +10,91 @@
 # modified 2013-01-03
 #
 
+use strict;
+use Getopt::Long;
+
+my ( $input_param, $output_param, $debug_param, $help_param, $version_param );
+my ( $result, $xml_file, $file_size );
+
+GetOptions(	'input|i=s'		=>	\$input_param,
+			'output|o=s'	=>	\$output_param,
+			'debug'			=>	\$debug_param,
+			'help|?'		=>	\$help_param,
+			'version'		=>	\$version_param );
+
+if ( $debug_param ) {
+	print "DEBUG: passed parameters:\n";
+	print "input_param: $input_param\n";
+	print "output_param: $output_param\n";
+	print "debug_param: $debug_param\n";
+	print "help_param: $help_param\n\n";
+}
+
+if ( $version_param ) {
+	print "parseTribuneDatabase.pl version 0\n";
+	exit;
+}
+
+if ( $help_param ) {
+	print "parsecatalog.pl\n\n";
+	print "version 0\n\n";
+	print "--input <filename>\n";
+	print "\tfile to process (required)\n";
+	print "--output <filename>\n";
+	print "\tif omitted, will be input file name with .csv extension\n";
+	print "--help | -?\n";
+	print "\tdisplay this text\n";
+	print "--version\n";
+	print "\tdisplay version information\n";
+	exit;
+}
+
+# if no input file is specified, grab the first command line parameter and use that
+if ( $input_param eq undef ) { $input_param = $ARGV[0]; }
+
+if ( $output_param eq undef ) {
+	$output_param = $input_param;
+	$output_param =~ s/\.(.+)$//;
+	$output_param .= ".csv"
+}
+
+if ( $debug_param ) {
+	print "DEBUG: passed parameters:\n";
+	print "input_param: $input_param\n";
+	print "output_param: $output_param\n";
+	print "debug_param: $debug_param\n";
+	print "help_param: $help_param\n\n";
+}
+
+if ( $debug_param ) { print "DEBUG: opening input file $input_param\n"; }
+open( INPUT_FILE, "<", $input_param )
+	or die "Can't open input file $input_param\n";
+
+$file_size = -s INPUT_FILE;
+if ( $debug_param ) { print "DEBUG: input file $input_param is $file_size bytes\n"; }
+
+if ( $debug_param ) { print "DEBUG: opening output file $output_param\n"; }
+open( OUTPUT_FILE, ">", $output_param )
+	or die "Can't create output file $output_param\n";
+
+$result = read( INPUT_FILE, $xml_file, $file_size );
+### do some error checking here
+
+# output the header row
+print OUTPUT_FILE "blah\t" .
+					"blah\t" .
+					"blah\n";
+
+##### need to figure out what our header is
+
+
+
+
+
+
+
+
+
 
 
 
